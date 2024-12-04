34eb895218098c17dc76fe0f69eff2db440232d1
Hal <haln_01@proton.me>
2024-12-04 14:19:15 -0800

    update template

--- src/bin/template.rs
+++ src/bin/template.rs
@@ -1,14 +1,24 @@
+#![allow(unused_variables, unused_imports)]
+
 use std::{
     env, fs,
-    io::{self, BufRead},
+    io::{self, BufRead, Read},
 };
 
 fn main() -> io::Result<()> {
     let args = env::args().collect::<Vec<String>>();
-    let file = fs::File::open(&args[1])?;
-    let reader = io::BufReader::new(file);
-    for line in reader.lines() {
-        println!("{}", line?);
-    }
+    let input = parse_input(&args[1]);
+    part_1(&input);
+    part_2(&input);
     Ok(())
+}
+
+fn part_1(input: &String) {}
+
+fn part_2(input: &String) {}
+
+fn parse_input(filename: &str) -> String {
+    let mut buf = String::new();
+    let _ = fs::File::open(filename).unwrap().read_to_string(&mut buf);
+    buf
 }
